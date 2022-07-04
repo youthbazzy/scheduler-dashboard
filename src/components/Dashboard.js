ed7945fe1580d56797d1e46f54846ab396a1d258
Bazil <king.bazil.khan@gmail.com>
2022-07-04 00:14:19 -0600

    Adds mock data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,31 @@ import Loading from "./Loading"
 
 import classnames from "classnames";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 export default class Dashboard extends Component {
+  
   state = {
     loading: false
   };
