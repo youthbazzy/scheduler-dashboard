17c8c602711cbab3a9270120bdc9c00e09afcbf9
Bazil <king.bazil.khan@gmail.com>
2022-07-04 00:11:53 -0600

    Imports loading component and returns loading if state is set to loading

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,12 +3,17 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Dashboard extends Component {
-  state = { loading: true }
+  state = {
+    loading: true
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
 }
-
-export default Dashboard;
