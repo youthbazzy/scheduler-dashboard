7c2dfd39ce5e764549e161651c3454cf144a6428
Bazil <king.bazil.khan@gmail.com>
2022-07-04 00:07:52 -0600

    Adds loading.js component;

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  state = { loading: true }
   render() {
     const dashboardClasses = classnames("dashboard");
 
